08abc6fef8111159cee76a263805292a2e15022d
hg340 <hg340@exeter.ac.uk>
2020-11-29 06:50:19 +0000

    parallel territory generation now working

--- R/PopCarryCap.R
+++ R/PopCarryCap.R
@@ -1,7 +1,13 @@
+
 terr_checks <- function(reach, river, terr_line,  terr_leng, t_length, buff, old_buff, new_buff, thresh, att){
 
   if (terr_leng > (t_length*(1-thresh)) && terr_leng < (t_length*(1+thresh))){
 
+    s_ord <- dplyr::pull(reach, var='Str_order')[1]
+
+    terr_line <- terr_line %>%
+      dplyr::mutate(Str_Ord = s_ord) %>%
+      dplyr::mutate(Terr_Leng =terr_leng)
     return(terr_line)
 
   } else {
@@ -21,18 +27,18 @@ terr_checks <- function(reach, river, terr_line,  terr_leng, t_length, buff, old
       }
 
     }
-    create_territories(reach, river, t_length, progbar=NULL, new_buff = recalc_buff, old_buff = buff, attempt = att)
+    create_territories(reach, river, t_length, new_buff = recalc_buff, old_buff = buff, attempt = att)
   }
 
 }
 
 
-create_territories <- function(reach, river, t_length, progbar=NULL, new_buff= NULL, old_buff=NULL, attempt=0) {
+create_territories <- function(reach, river, t_length=NULL,  new_buff= NULL, old_buff=NULL, attempt=0) {
 
-
-  # if (!is.null(progbar)){
-  #   progbar$tick()
-  # }
+  if (is.null(t_length)){
+    # Here the random territory size is generated - based on Literature - add details...
+    t_length <- rnorm(1, 1630, 293)
+  }
 
   if (is.null(new_buff)){
     buff <- t_length/2
@@ -50,7 +56,7 @@ create_territories <- function(reach, river, t_length, progbar=NULL, new_buff= N
     sf::st_buffer(0.1)
 
 
-  if (st_geometry_type(terr_line)=='MULTIPOLYGON'){
+  if (sf::st_geometry_type(terr_line)=='MULTIPOLYGON'){
     att <- attempt + 1
     terr_line <- sf::st_cast(terr_line, to= 'POLYGON') %>%
       dplyr::mutate(area = sf::st_area(.)) %>%
@@ -60,76 +66,104 @@ create_territories <- function(reach, river, t_length, progbar=NULL, new_buff= N
       dplyr::summarise(across(c("BFI_40m", "BDC"), ~ weighted.mean(.x, w= Leng, na.rm = TRUE)))%>%
       sf::st_buffer(0.1)
 
-    terr_leng <- as.numeric(lwgeom::st_perimeter(st_buffer(terr_line, 0.1)))/2
+    terr_leng <- as.numeric(lwgeom::st_perimeter(sf::st_buffer(terr_line, 0.1)))/2
 
     if ( att >=5){
       stop('Cannot generate territory of accecptable size - discard for reach.')
     }
 
+    #threshold of within 40% of target applied to allow adjustment of territories.
+    # max and min will still fall within max and min values reported in literature.
     return(terr_checks(reach, river, terr_line,  terr_leng, t_length, buff, old_buff, new_buff, thresh = 0.4, att=att))
-
-
-    # if (terr_leng > (t_length*0.6) && terr_leng < (t_length*1.4)){
-    #   return(terr_line)
-    # } else {
-    #   create_territories(reach, river, t_length, progbar=NULL, new_buff = recalc_buff, old_buff = buff, attempt = att)
-    # }
 
   }
 
   terr_leng <- as.numeric(lwgeom::st_perimeter(st_buffer(terr_line, 0.1)))/2
-  # message(terr_leng)
 
   return(terr_checks(reach, river, terr_line,  terr_leng, t_length, buff, old_buff, new_buff, thresh = 0.05, att=0))
-  # if (terr_leng > (t_length*0.95) && terr_leng < (t_length*1.05)){
-  #
-  #   return(terr_line)
-  #
-  # } else {
-  #
-  #   if (is.null(new_buff)){
-  #     if (terr_leng < t_length){
-  #       recalc_buff <- buff * 1.5
-  #     } else {
-  #       recalc_buff <- buff * 0.5
-  #     }
-  #
-  #   } else {
-  #     if (terr_leng < t_length){
-  #       recalc_buff <- buff + ((abs(old_buff-buff))/2)
-  #     } else {
-  #       recalc_buff <- buff - ((abs(old_buff-buff))/2)
-  #     }
-  #
-  #   }
-  #   create_territories(reach, river, t_length, progbar=NULL, new_buff = recalc_buff, old_buff = buff)
-  # }
-
-}
-
-gen_terr_safe <- function() {
-  progbar$tick()
-
-    purrr::safely(create_territories(., BeaverNetwork, 1000, progbar = pb))
 
 }
 
 
-
-#' recursive function to create territory channel length to within 5% of target
+#' Caller function for create territories wrapped in safely.
 #'
+#' @import foreach doParallel parallel progress
 #' @export
 gen_territories <- function(BeaverNetwork){
-  pb <- progress::progress_bar$new(total = nrow(BeaverNetwork),
-                                   clear = FALSE)
+  oldw <- getOption("warn")
+  options(warn = -1)
 
-  out <- BeaverNetwork %>%
-    tibble::rowid_to_column(., var='id') %>%
-    group_by(id) %>%
-    group_split() %>%
-    purrr::map(., ~ purrr::safely(create_territories(., BeaverNetwork, 1000, progbar = pb))) #%>%
-    #dplyr::bind_rows()
-  print('BREAK')
+  #setup parallel backend to use many processors
+
+  # cores <- 10
+  cores=parallel::detectCores()
+  cl <- parallel::makeCluster(cores[1]-2) #not to overload your computer
+
+  cl <- parallel::makeCluster(cores, cores = cores)
+
+  doParallel::registerDoParallel(cl)
+
+  iter_size <- nrow(BeaverNetwork)
+
+
+  # iter_size= 100
+  n <- ceiling(iter_size/cores)
+
+
+  # st <- seq(1,iter_size,by=n)
+  # en <- c(seq(n,iter_size,by=n),iter_size)
+
+  split_df <- BeaverNetwork %>%
+    dplyr::group_by((dplyr::row_number()-1) %/% (dplyr::n()/cores)) %>%
+    dplyr::group_split()
+
+  # pb <- progress::progress_bar$new(total = nrow(BeaverNetwork),
+  #                                  clear = FALSE)
+
+  gen_terr_safe <- function(x) {
+    # pb$tick()
+
+    f = purrr::safely(function() create_territories(reach = x, river = BeaverNetwork))
+
+    f()
+
+  }
+  # run paralell territory generation...
+  out <- foreach::foreach(i = seq_along(split_df), .combine = rbind,
+                         .packages = c("dplyr", "sf", "purrr", "lwgeom", "tibble", "magrittr"),
+                         .export = c("gen_terr_safe", "create_territories", "terr_checks")) %dopar% {
+
+
+
+
+
+                           par_terrs <- split_df[[i]] %>%
+                             tibble::rowid_to_column(., var='id') %>%
+                             dplyr::group_by(id) %>%
+                             dplyr::group_split() %>%
+                             purrr::map( ~ gen_terr_safe(.)) %>%
+                             purrr::map(., ~ .$result) %>%
+                             dplyr::bind_rows()
+
+                           return(par_terrs)
+                         }
+
+  parallel::stopCluster(cl)
+
+  options(warn = oldw)
+
+  out <- out %>%
+    tibble::rowid_to_column(., var='id')
+
+  imp_terr_n <- nrow(BeaverNetwork) -nrow(out)
+
+  if (imp_terr_n > 0) {
+    warning(sprintf('Territories could not be generated for %s reaches',
+                    imp_terr_n))
+  }
+
+
+  return(out)
 }
 
 
---
+++ R/doparimport.R
@@ -0,0 +1,7 @@
+#re-export dopar operator
+#
+#@importFrom foreach %dopar%
+#@name %dopar%
+#@rdname dopar
+# @export
+# NULL
