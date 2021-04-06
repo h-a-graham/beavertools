8d894cf79b03ac78c9840df67102eb13e04fe968
hg340 <hg340@exeter.ac.uk>
2021-04-06 20:03:55 +0100

    Big progress on management impacts on pop - plots now working quite nicely..

--- R_Otter_workflow/3_Pop_expansion_predictions/Pop_expansion_predict.R
+++ R_Otter_workflow/3_Pop_expansion_predictions/Pop_expansion_predict.R
@@ -90,6 +90,8 @@ mid_cap <- function() lower_capacity + ((upper_capacity-lower_capacity)*0.5)
 hacked_df %>%
   # filter(cap_name==115)%>%
   ggplot(., aes(x=year_adj, y=.fitted))+
+
+  ### This bit adds the ribbon to show estimated maximum  terr. capacity.
   # geom_ribbon(data=ribbon_df(), aes(ymin=lower_capacity, ymax=upper_capacity, xmin=2000),
   #             fill='grey90', size=0.1, alpha=0.2, linetype=2, color="grey10") +
   # annotate("text", x=2020, y = mid_cap(),
@@ -98,14 +100,27 @@ hacked_df %>%
   #              arrow = arrow(length = unit(0.01, "npc")),lwd=0.5, color="grey20") +
   # geom_segment(aes(x = 2020, y = mid_cap() - 5, xend = 2020, yend = lower_capacity +1),
   # arrow = arrow(length = unit(0.01, "npc")), lwd=0.5, color="grey20") +
-  geom_ribbon(aes(ymin=pred.lwr, ymax = pred.upr, group=reorder(cap_name, rev(cap_name)), fill=cap_name), colour=NA) +
-  stat_summary(fun = mean, geom = 'line', size=0.6, alpha=0.6, linetype=1, color="grey20") +
-  stat_summary(aes(y=pred.lwr), fun = min, geom = 'line', size=0.3, alpha=0.6, linetype=1, color="grey20") +
-  stat_summary(aes(y=pred.upr),fun = max, geom = 'line', size=0.3, alpha=0.6, linetype=1, color="grey20") +
-  scale_fill_continuous_sequential("TealGrn", rev=F) +
-  guides(fill = guide_colourbar(barwidth = 8, barheight = 0.5, title="Territory Capacity")) +
-  geom_point(data=terr_counts, aes(x=year_adj, y=terr_count), inherit.aes = F, alpha=0.6, colour='grey10')+
 
+  ### This gives just the lines
+  geom_line(aes(group=reorder(cap_name, rev(cap_name)), color=cap_name), lwd=1.2, alpha=0.7)+
+  stat_summary(fun = mean, geom = 'line', size=0.6, alpha=0.6, linetype=1, color="grey20") +
+  # stat_summary(fun = min, geom = 'line', size=0.4, alpha=0.6, linetype=2, color="grey20") +
+  # stat_summary(fun = max, geom = 'line', size=0.4, alpha=0.6, linetype=2, color="grey20") +
+  scale_colour_continuous_sequential("Batlow", rev=F) +
+  guides(colour = guide_colourbar(barwidth = 8, barheight = 0.5, title="Territory Capacity")) +
+
+  ## this generates the Confidence interval version (CIs are very debatable - safer to levave?)
+  # geom_ribbon(aes(ymin=pred.lwr, ymax = pred.upr, group=reorder(cap_name, rev(cap_name)), fill=cap_name), colour=NA) +
+  # stat_summary(aes(y=pred.lwr), fun = min, geom = 'line', size=0.3, alpha=0.6, linetype=1, color="grey20") +
+  # stat_summary(aes(y=pred.upr),fun = max, geom = 'line', size=0.3, alpha=0.6, linetype=1, color="grey20") +
+  # scale_fill_continuous_sequential("TealGrn", rev=F) +
+  # guides(fill = guide_colourbar(barwidth = 8, barheight = 0.5, title="Territory Capacity")) +
+
+  #adds mean model to plot
+  stat_summary(fun = mean, geom = 'line', size=0.6, alpha=0.6, linetype=1, color="grey20") +
+  # adds original data
+  geom_point(data=terr_counts, aes(x=year_adj, y=terr_count), inherit.aes = F, colour='grey5')+
+  # define plot style n stuff
   coord_cartesian(ylim=c(0,upper_capacity +5), xlim = c(2007, 2045))+
   labs(x = 'Year', y="Number of Territories")+
   theme_bw() +
@@ -127,9 +142,9 @@ long_df <- hacked_df %>%
 pop.dynams <- function(df, x_val, x_lab, leg_pos){
   ggplot(df, aes(x=!! dplyr::sym(x_val) , y=mid.long , colour=cap_name))+
     # geom_ribbon(aes(ymin=lwr.long, ymax = upr.long, group=reorder(cap_name, rev(cap_name)), fill=cap_name), lwd=0.9) +
-    geom_line(aes(group=c(reorder(cap_name, rev(cap_name)))),lwd=1.6) +
+    geom_line(aes(group=c(reorder(cap_name, rev(cap_name)))),lwd=0.8) +
     # scale_fill_continuous_sequential("Batlow", rev=F) +
-    scale_colour_continuous_sequential("TealGrn", rev=F) +
+    scale_colour_continuous_sequential("Batlow", rev=F) +
     guides(colour = guide_colourbar(barwidth = 8, barheight = 0.5, title="Territory Capacity")) +
     labs(x = x_lab, y='')+
     theme_bw() +
@@ -148,5 +163,103 @@ long_df %>%
   theme(strip.background = element_blank(), strip.text = element_blank()) +
   ggsave(file.path(plot_dir, 'TerritoryDynamics.png'),
          dpi=300, height=7, width=10)
+
+
+# ------ management impacts ----------------
+# okay let's assume that in 2025 we start removing 5 territories each year...
+
+
+mgmt_scenario <- function(df, .mgmt_start, .mgmt_n_terrs) {
+
+  gen_mgmt_curve <- function(mgmt_start, mgmt_n_terrs){
+    growth_vec <- vector("double", length(nrow(df)))
+
+    for (i in seq_len(nrow(df))) {
+
+      if (df$year_adj[i] < mgmt_start){
+        g <- round(df$.fitted[i],2)
+
+      } else{
+        g_rate <- df$n_terr_growth[which.min(abs(df$.fitted - g))]
+
+        g <- g + g_rate - mgmt_n_terrs
+      }
+      growth_vec[i] <- g
+    }
+
+    return(tibble(mgmt_growth = growth_vec,
+                  mgmt_year = mgmt_start,
+                  mgmt_removed = mgmt_n_terrs) %>%
+             bind_cols(df))
+  }
+
+  dist_func <- function(.year, .nList){
+    purrr::map(.nList, ~gen_mgmt_curve(.year, .)) %>%
+      bind_rows()
+  }
+
+  purrr::map(.mgmt_start, ~dist_func(., .mgmt_n_terrs)) %>%
+    bind_rows()%>%
+    mutate_if(is.numeric, ~replace(., .<0, NA))
+
+}
+
+
+
+
+mgmt_df <- hacked_df %>%
+  group_by(cap_name) %>%
+  group_split() %>%
+  purrr::map(., ~ mgmt_scenario(., c(2022, 2025, 2030), c(5, 7, 10, 15))) %>%
+  bind_rows()
+
+
+# create plot.
+mgmt_df %>%
+  # filter(cap_name==115)%>%
+  ggplot(., aes(x=year_adj, y=.fitted))+
+
+  # shows range of unmanaged scenario
+  stat_summary(fun = min, geom = 'line', size=0.4, alpha=0.6, linetype=2, color="grey20") +
+  stat_summary(fun = max, geom = 'line', size=0.4, alpha=0.6, linetype=2, color="grey20") +
+  ### This gives just the lines
+  geom_line(aes(y=mgmt_growth, group=reorder(cap_name, rev(cap_name)), color=cap_name), lwd=1, alpha=0.5)+
+  scale_colour_continuous_sequential("Batlow", rev=F) +
+  guides(colour = guide_colourbar(barwidth = 8, barheight = 0.5, title="Territory Capacity")) +
+
+  facet_grid(mgmt_removed ~ mgmt_year ) +
+
+  # define plot style n stuff
+  coord_cartesian(ylim=c(0,upper_capacity +5), xlim = c(2007, 2045))+
+  labs(x = 'Year', y="Number of Territories")+
+  theme_bw() +
+  theme(legend.position = "bottom",
+        axis.title.y = element_text(margin = margin(t = 0, r = 3, b = 0, l = 0)),
+        axis.title.x = element_text(margin = margin(t = 3, r = 0, b = 0, l = 0)))+
+  ggsave(file.path(plot_dir, 'MgmtDynamics.png'),
+         dpi=300, height=7, width=10)
+
+
+
+
+
+# plotting option with ribbon.
+# mgmt_df %>%
+#   filter(cap_name %in% c(min(hacked_df$cap_name),max(hacked_df$cap_name))) %>%
+#   mutate(cap_name = ifelse(cap_name == min(hacked_df$cap_name), 'Low', 'High')) %>%
+#   select(cap_name, .fitted, year_adj, mgmt_scen) %>%
+#   pivot_longer(., cols=c(.fitted, mgmt_scen), names_to = "type", values_to = 'value')%>%
+#   pivot_wider(., names_from =cap_name, values_from = c(value)) %>%
+#   mutate(type = ifelse(type=='.fitted', 'No Management', ' With Management'))%>%
+#   ggplot(., aes(x=year_adj, fill=type)) +
+#   geom_ribbon(aes(ymin=Low, ymax=High, linetype=type), alpha=0.7, colour='grey50') +
+#   scale_fill_manual(values=c("#43FFB5", NA)) +
+#   coord_cartesian(ylim=c(0,upper_capacity +5), xlim = c(2007, 2045))+
+#   labs(x = 'Year', y="Number of Territories")+
+#   theme_bw() +
+#   theme(legend.position = "bottom",
+#         legend.title = element_blank(),
+#         axis.title.y = element_text(margin = margin(t = 0, r = 3, b = 0, l = 0)),
+#         axis.title.x = element_text(margin = margin(t = 3, r = 0, b = 0, l = 0)))
 
 
