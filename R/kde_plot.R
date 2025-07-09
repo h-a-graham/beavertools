f926cb716aaed357f370264808397cd59ff6e40d
Hugh Graham <hugh.a.graham@gmail.com>
2025-07-09 22:51:03 +0100

    linting and drop raster

--- R/kde_plot.R
+++ R/kde_plot.R
@@ -40,18 +40,38 @@
 #' plot_forage_density(ROBT_201920, catchment = RivOtter_Catch_Area, rivers = TRUE,
 #'                                      trans_fill=TRUE)
 #'
-plot_forage_density <- function(kd_raster, basemap=TRUE, basemap_type = "osmgrayscale", trans_fill = TRUE, trans_type = 'log10',
-                     axes_units = TRUE, scalebar=TRUE, scalebar_loc = 'tl', north_arrow = TRUE, north_arrow_loc = 'br', north_arrow_size = 0.75,
-                     wgs=TRUE, guide=TRUE, catchment=NULL, rivers=FALSE, add_hillshade = FALSE, plot_extent=NULL, attribute = TRUE, guide_width =NULL,
-                     mask_fill="grey50"){
-
+plot_forage_density <- function(
+  kd_raster,
+  basemap = TRUE,
+  basemap_type = "osmgrayscale",
+  trans_fill = TRUE,
+  trans_type = 'log10',
+  axes_units = TRUE,
+  scalebar = TRUE,
+  scalebar_loc = 'tl',
+  north_arrow = TRUE,
+  north_arrow_loc = 'br',
+  north_arrow_size = 0.75,
+  wgs = TRUE,
+  guide = TRUE,
+  catchment = NULL,
+  rivers = FALSE,
+  add_hillshade = FALSE,
+  plot_extent = NULL,
+  attribute = TRUE,
+  guide_width = NULL,
+  mask_fill = "grey50"
+) {
   orig_crs <- sf::st_crs(kd_raster)
-  kd_raster <- raster::projectRaster(kd_raster, crs=sp::CRS(sprintf('+init=EPSG:%s',sf::st_crs(4326)$epsg)))
+  kd_raster <- terra::project(
+    kd_raster,
+    "EPSG:4326"
+  )
 
   # define extent
   set_lims <- TRUE
 
-  if (is.null(plot_extent)){
+  if (is.null(plot_extent)) {
     set_lims <- FALSE
   } else {
     plot_extent <- create_plot_ext(plot_extent)
@@ -59,79 +79,120 @@ plot_forage_density <- function(kd_raster, basemap=TRUE, basemap_type = "osmgray
 
   p <- ggplot2::ggplot()
 
-  if (isTRUE(basemap)){
-    p <- p + ggspatial::annotation_map_tile(type = basemap_type, zoomin = 0, alpha = 0.8)
+  if (isTRUE(basemap)) {
+    p <- p +
+      ggspatial::annotation_map_tile(
+        type = basemap_type,
+        zoomin = 0,
+        alpha = 0.8
+      )
   }
 
-  if (isTRUE(add_hillshade)){
+  if (isTRUE(add_hillshade)) {
     p <- p + ggspatial::annotation_map_tile(type = 'hillshade', zoomin = 0)
   }
 
-  if (!is.null(catchment)){
+  if (!is.null(catchment)) {
     catchment <- check_spatial_feature(catchment, 'catchment')
-    catch_mask <- create_mask(catchment)%>%
+    catch_mask <- create_mask(catchment) %>%
       sf::st_transform(crs = 4326)
-    p <- p + ggspatial::annotation_spatial(catch_mask, fill = mask_fill, alpha=0.5)
+    p <- p +
+      ggspatial::annotation_spatial(catch_mask, fill = mask_fill, alpha = 0.5)
   }
 
-  if (isTRUE(rivers)){
+  if (isTRUE(rivers)) {
     if (is.null(catchment)) {
-      message('"rivers" argument ignored. A valid "catchment" area is required.')
+      message(
+        '"rivers" argument ignored. A valid "catchment" area is required.'
+      )
     } else {
       river_sf <- get_rivers(catchment)
-      p <- p + ggspatial::annotation_spatial(river_sf, colour = "#5699FA", alpha=0.9, size=0.2)
+      p <- p +
+        ggspatial::annotation_spatial(
+          river_sf,
+          colour = "#5699FA",
+          alpha = 0.9,
+          size = 0.2
+        )
     }
-  } else if(class(rivers)[1] == "sf"){
+  } else if (class(rivers)[1] == "sf") {
     rivers <- rivers %>%
       sf::st_transform(crs = 4326)
-    p <- p + ggspatial::annotation_spatial(rivers, colour = "#5699FA", alpha=0.9, size=0.2)
+    p <- p +
+      ggspatial::annotation_spatial(
+        rivers,
+        colour = "#5699FA",
+        alpha = 0.9,
+        size = 0.2
+      )
   }
 
-  p <- p + ggspatial::layer_spatial(kd_raster, ggspatial::aes(fill=stat(band1)),alpha = 0.7) +
+  p <- p +
+    ggspatial::layer_spatial(
+      kd_raster,
+      ggspatial::aes(fill = ggplot2::after_stat(band1)),
+      alpha = 0.7
+    ) +
     ggplot2::theme_bw() +
-    ggplot2::theme(legend.title=ggplot2::element_text(size=10))
+    ggplot2::theme(legend.title = ggplot2::element_text(size = 10))
 
-
-  if (isTRUE(trans_fill)){
-    p <- p + ggplot2::scale_fill_viridis_c(na.value = NA, name= sprintf('%s Forage Density', trans_type), trans=trans_type,
-                                           n.breaks=2)
+  if (isTRUE(trans_fill)) {
+    p <- p +
+      ggplot2::scale_fill_viridis_c(
+        na.value = NA,
+        name = sprintf('%s Forage Density', trans_type),
+        trans = trans_type,
+        n.breaks = 2
+      )
   } else {
-    p <- p + ggplot2::scale_fill_viridis_c(na.value = NA, name='Forage Density')
+    p <- p +
+      ggplot2::scale_fill_viridis_c(na.value = NA, name = 'Forage Density')
   }
 
-  if (isFALSE(axes_units)){
-    p <- p + ggplot2::theme(axis.title.x=ggplot2::element_blank(),
-                   axis.text.x=ggplot2::element_blank(),
-                   axis.ticks.x=ggplot2::element_blank(),
-                   axis.title.y=ggplot2::element_blank(),
-                   axis.text.y=ggplot2::element_blank(),
-                   axis.ticks.y=ggplot2::element_blank())
+  if (isFALSE(axes_units)) {
+    p <- p +
+      ggplot2::theme(
+        axis.title.x = ggplot2::element_blank(),
+        axis.text.x = ggplot2::element_blank(),
+        axis.ticks.x = ggplot2::element_blank(),
+        axis.title.y = ggplot2::element_blank(),
+        axis.text.y = ggplot2::element_blank(),
+        axis.ticks.y = ggplot2::element_blank()
+      )
   }
 
   if (isTRUE(scalebar)) {
-    p <- p + ggspatial::annotation_scale(location= scalebar_loc)
+    p <- p + ggspatial::annotation_scale(location = scalebar_loc)
   }
 
   if (isTRUE(north_arrow)) {
     p <- p +
-      ggspatial::annotation_north_arrow(location = north_arrow_loc, which_north = "true",
-                                               height = ggplot2::unit(north_arrow_size, "cm"),
-                                               width = ggplot2::unit(north_arrow_size, "cm"),
-                                               style = ggspatial::north_arrow_orienteering(text_col=NA,
-                                                                                           fill = c("black", "black")))
+      ggspatial::annotation_north_arrow(
+        location = north_arrow_loc,
+        which_north = "true",
+        height = ggplot2::unit(north_arrow_size, "cm"),
+        width = ggplot2::unit(north_arrow_size, "cm"),
+        style = ggspatial::north_arrow_orienteering(
+          text_col = NA,
+          fill = c("black", "black")
+        )
+      )
   }
 
-  if (isTRUE(set_lims) && isTRUE(wgs)){
+  if (isTRUE(set_lims) && isTRUE(wgs)) {
     # p <- p + ggplot2::scale_x_continuous(limits= c(plot_extent[1], plot_extent[2])) +
     #   ggplot2::scale_y_continuous(limits =c(plot_extent[3], plot_extent[4]))
 
-    p <- p + ggplot2::coord_sf(xlim=c(plot_extent[1], plot_extent[2]), ylim=c(plot_extent[3], plot_extent[4]),
-                      crs=sf::st_crs(kd_raster))
+    p <- p +
+      ggplot2::coord_sf(
+        xlim = c(plot_extent[1], plot_extent[2]),
+        ylim = c(plot_extent[3], plot_extent[4]),
+        crs = sf::st_crs(kd_raster)
+      )
   }
 
   if (isFALSE(wgs)) {
     if (isTRUE(set_lims)) {
-
       pe <- sf::st_bbox(plot_extent)
       pe[[1]] <- plot_extent[1]
       pe[[2]] <- plot_extent[3]
@@ -139,38 +200,38 @@ plot_forage_density <- function(kd_raster, basemap=TRUE, basemap_type = "osmgray
       pe[[4]] <- plot_extent[4]
 
       pe <- sf::st_as_sfc(pe) %>%
-        sf::st_set_crs(sf::st_crs(kd_raster))%>%
+        sf::st_set_crs(sf::st_crs(kd_raster)) %>%
         sf::st_transform(orig_crs) %>%
-        st_bbox() %>%
+        sf::st_bbox() %>%
         define_extent_bbox()
 
-      p <- p + ggplot2::coord_sf(xlim=c(pe[1], pe[2]), ylim=c(pe[3], pe[4]),
-                                 crs = orig_crs, datum = orig_crs)
-    } else{
+      p <- p +
+        ggplot2::coord_sf(
+          xlim = c(pe[1], pe[2]),
+          ylim = c(pe[3], pe[4]),
+          crs = orig_crs,
+          datum = orig_crs
+        )
+    } else {
       p <- p + ggplot2::coord_sf(crs = orig_crs, datum = orig_crs)
     }
-
   }
 
   if (isFALSE(guide)) {
-    p <- p + ggplot2::guides(fill=FALSE)
+    p <- p + ggplot2::guides(fill = FALSE)
   }
 
-  if (!is.null(guide_width)){
+  if (!is.null(guide_width)) {
     p <- p + ggplot2::guides(fill = guide_colourbar(barheight = guide_width))
   }
 
-  if (isTRUE(basemap)|isTRUE(rivers)|isTRUE(add_hillshade)){
-    if (isTRUE(attribute)){
+  if (isTRUE(basemap) | isTRUE(rivers) | isTRUE(add_hillshade)) {
+    if (isTRUE(attribute)) {
       p <- p +
         ggplot2::labs(caption = '© OpenStreetMap contributors') +
-        ggplot2::theme(plot.caption = ggplot2::element_text(size=6))
+        ggplot2::theme(plot.caption = ggplot2::element_text(size = 6))
     }
   }
 
-
   return(p)
 }
-
-
-
